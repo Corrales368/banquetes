f0ae26e8063274a279150c4bf7d99d3adca2cf8b
santiagocorralesv <santi368444110@gmail.com>
2023-04-23 16:56:09 -0500

    add view error pages

--- apps/shared/views.py
+++ apps/shared/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.template import RequestContext
 
-# Create your views here.
+
+def handler404(request, *args, **argv):
+    response = render('404.html', {}, context_instance=RequestContext(request))
+    response.status_code = 404
+    return response
+
+
+def handler500(request, *args, **argv):
+    response = render('500.html', {}, context_instance=RequestContext(request))
+    response.status_code = 500
+    return response
